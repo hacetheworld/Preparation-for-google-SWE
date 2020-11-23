7b529bdf6c63daaef54291cbf9a52db1d4bd2a6e
Ajay <majay1638@gmail.com>
2020-11-23 15:12:33 +0530

    added some questions

--- 150 random Data Structure  Questions/graph/build-graph.py
+++ 150 random Data Structure  Questions/graph/build-graph.py
@@ -2,5 +2,5 @@ def adjList(arr, v):
     adjList = [[] for _ in range(v)]
     for u in arr:
         adjList[u[0]].append(u[1])
-        adjList[u[1]].append(u[0])
+        # adjList[u[1]].append(u[0])
     return adjList
